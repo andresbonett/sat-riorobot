23c09f632b1d8abfe2fa4ada130d0de0540838a6
Andres Bonett <andresbonettm@gmail.com>
2020-10-24 14:41:55 -0500

    add axios

--- src/app.js
+++ src/app.js
@@ -1,66 +1,120 @@
-const SerialPort = require('serialport')
+const SerialPort = require("serialport");
+var axios = require("axios");
 
-const btn = document.getElementById('scan')
-const list = document.getElementById('listPort')
-const renderNode = document.getElementById('ports')
+const btn = document.getElementById("boton");
+const btn_online = document.getElementById("online");
+const renderNode = document.getElementById("nivel");
 
-let port = ''
+let port = "";
+let online = false;
+const Serial = "5CG9411N9S";
+const URL = "https://api.riorobot.com.co/sensor/1";
+// const URL = "http://localhost:3000/sensor";
 
-btn.addEventListener('click', async () => {
-  const data = await listPort()
-  list.innerHTML = data[0].manufacturer ? templateDevice(data[0]) : 'Error'
-})
+btn_online.addEventListener("click", async () => {
+  online = online ? false : true;
+  console.log(online);
+  btn_online.textContent = online ? "ON" : "OFF";
+});
+
+btn.addEventListener("click", async () => {
+  renderNode.innerHTML = loader;
+  if (port === "") {
+    const data = await listPort();
+    // list.innerHTML = data[0].manufacturer ? templateDevice(data[0]) : "Error";
+    port = data.comName;
+    renderLectura();
+  } else {
+    console.log("port is open");
+  }
+});
 
 async function listPort() {
-  let list = []
+  let list = [];
   await SerialPort.list((err, ports) => {
-    console.log(ports[0])
     if (err) {
-      console.log(err.message)
-      return err.message
+      console.log(err.message);
+      return err.message;
     }
 
     if (ports.length === 0) {
-      return 'No ports discovered'
+      return "No ports discovered";
     }
-    list = [...ports]
-  })
-  return list
+    list = ports[0]; // primer puerto
+    // list = [...ports]; // lista de puertos
+  });
+  return list;
 }
 
 // template
 
-function templateDevice(device) {
-  port = device.comName
-  return /*html*/ `
-  <h1 id="status">${device.manufacturer}</h1>
-  <button onclick="renderLectura()">Status</button>
-  `
-}
+// function templateDevice(device) {
+//   port = device.comName;
+//   return /*html*/ `
+//   <h1 id="status">${device.manufacturer}</h1>
+//   <button onclick="renderLectura()">Status</button>
+//   `;
+// }
 
 // Leer datos
 function renderLectura() {
-  var arduinoCOMPort = port
+  var arduinoCOMPort = port;
 
   const arduinoSerialPort = new SerialPort(arduinoCOMPort, {
     baudRate: 9600,
-  })
+  });
 
-  arduinoSerialPort.on('open', function () {
-    console.log('Serial Port ' + arduinoCOMPort + ' is opened.')
-  })
+  arduinoSerialPort.on("open", function () {
+    console.log("Serial Port " + arduinoCOMPort + " is opened.");
+  });
 
-  arduinoSerialPort.on('data', function (data) {
+  arduinoSerialPort.on("data", function (data) {
     // let value = data.toString('utf8')
-    let value = parseInt(data.toString('utf8'))
+    let value = parseInt(data.toString("utf8"));
     if (value) {
-      // sendData(value)
-      renderData(value)
+      renderData(value);
+      if (online) {
+        sendData(value);
+      }
     }
-  })
+  });
 
   function renderData(value) {
-    console.log(`Distancia: ${value}cm`)
-    renderNode.innerHTML = `Distancia: ${value}cm`
+    renderNode.innerHTML = value;
   }
 }
+
+function sendData(value) {
+  const d = new Date();
+  const Fecha =
+    [d.getFullYear(), d.getMonth() + 1, d.getDate()].join("-") +
+    " " +
+    [d.getHours(), d.getMinutes(), d.getSeconds()].join(":");
+
+  const Valor = parseInt(value);
+  const data = JSON.stringify({
+    Fecha,
+    Serial,
+    Valor,
+  });
+  //var data = "{ Fecha: '2020-10-5 13:18:35', Serial: '5CG9411N9S', Valor: 21 }";
+
+  var config = {
+    method: "post",
+    url: URL,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data: data,
+  };
+
+  axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+const loader = `<div class="lds-grid"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>`;
